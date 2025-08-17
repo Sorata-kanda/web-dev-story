46d2d429b7e9473a9b72704fe2d5c059f204a964
sorata-kanda <jangraboy.nature@gmail.com>
2025-08-17 23:54:04 +0530

    Day - 1 // of JS started

---
+++ Javascript/basics.js
@@ -0,0 +1,8 @@
+// understood REPL [Read-Evaluate-Print-Loop]
+// learn - variables, operations, NaN
+
+/* As i am just starting out we are not using vs code but instead
+I am using chrome console! And as console is a temporary environment
+unfortunately i can't share that code, but that code is BASICS only 
+:)
+*/ 
