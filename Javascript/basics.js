af1278e7d0ac4d7cff588401278fe0f4f2904c3d
sorata-kanda <jangraboy.nature@gmail.com>
2025-08-22 23:57:21 +0530

    Day - 1//on lec 18

--- Javascript/basics.js
+++ Javascript/basics.js
@@ -1,5 +1,13 @@
 // understood REPL [Read-Evaluate-Print-Loop]
 // learn - variables, operations, NaN
+//operators percidence:- () , ** , *,/,% , +, -
+//assiemnt operators: +=, -=, *=, /=, %=, **=, ++, --
+//unary operators: ++, --, typeof
+//identifiers
+//we can cahnge type in js
+
+
+
 
 /* As i am just starting out we are not using vs code but instead
 I am using chrome console! And as console is a temporary environment
