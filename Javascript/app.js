30e5aeaf245b4d5fa18f697038bbe56fa222be82
sorata-kanda <jangraboy.nature@gmail.com>
2025-08-24 23:58:55 +0530

    Day - 2// started // on 5th lec

---
+++ Javascript/app.js
@@ -0,0 +1,44 @@
+// console.log("hello world");
+// console.log("Shirou neko");
+
+let a = 10;
+let b = 5;
+console.log("sum is: ", a + b);
+
+
+let pencil = 10;
+let eraser = 5;
+let output = "Total price: "+ (pencil + eraser) +" $"
+// console.log("Total price: ", pencil + eraser+"$"); // comma (,) will give a space while plus(+) doesn't
+console.log(output);
+//using template litrals
+console.log(`Total price by template litreal: ${pencil + eraser} ruppes`);
+
+
+
+//Operators
+
+//Operators arithemetic 
+let  aa = 10;
+let bb = 5;
+console.log(a+b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
+
+//Operators unary 
+console.log("\n");
+console.log(aa++);
+console.log(aa--);
+console.log(++aa);
+console.log(++aa);
+
+
+//Operators assiment
+
+b = a; //+=, -=, /=, *=
+console.log("\n");
+console.log(b);
+
