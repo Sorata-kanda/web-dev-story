5cb01d6586b0764aaa8aec9b35641ceca18730d2
sorata-kanda <jangraboy.nature@gmail.com>
2025-08-25 23:58:43 +0530

    Day - 2//on lec - 14

--- Javascript/app.js
+++ Javascript/app.js
@@ -42,3 +42,74 @@ b = a; //+=, -=, /=, *=
 console.log("\n");
 console.log(b);
 
+
+//comparison operators
+
+let age = 18; //>= , <= , < , ==, !=
+console.log(age>18);
+
+//Important things you should know!
+
+let str = '5';
+let num = 5;
+console.log(str==num); //This will just check the values
+console.log(str===num); //This will check the datatype
+ 
+console.log(0 == ' '); //This is true
+console.log(null == undefined); //True
+console.log(null === undefined); //False
+
+console.log(0 == false); //True 
+console.log(0 === false); //False
+
+
+//comparison for non - numbers
+console.log("\n");
+console.log('a' < 'b');
+console.log('a' < 'A'); //false //uniques value know as {unicode}
+console.log('*' < '&');
+console.log('*' > '&');
+
+
+//conditional statments
+
+console.log("\n");
+let agee = 23;
+if(agee >= 18){
+    console.log("you are eligible to vote");
+}else{
+    console.log("you are not eligible to vote");
+}
+
+
+//question :- make a traffic light system
+
+let color = "blue";
+
+if(color == "red"){
+    console.log("STOP!");
+}
+else if(color == "yellow"){
+    console.log("You should be ready to go");
+}else if(color == "green"){
+    console.log("You can go!");
+}else{
+    console.log("NOT A VALID LIGHT");
+}
+
+//popcorn question:-
+console.log("\n");
+let size = "XL";
+
+if(size === "XL"){
+    console.log(`price: Rs.${250}`);
+}else if(size === "L"){
+    console.log(`price: Rs.${200}`);
+}else if(size === "M"){
+    console.log(`price: Rs.${100}`);
+}else if(size === "S"){
+    console.log(`price: Rs.${50}`);
+}else{
+    console.log(`Required packet doesn't exists`);
+} 
+
