dc4d806b52b1b5addae3937a12336d10b6b595b4
sorata-kanda <jangraboy.nature@gmail.com>
2025-10-28 18:30:00 +0530

    P-31. D-12 completed

--- Javascript/Intermidiate~basic/advanced js/app4.js
+++ Javascript/Intermidiate~basic/advanced js/app4.js
@@ -1,25 +1,72 @@
 // JSON -> Js objects
 
-let jsonRes = 
-'{"fact":"The first true cats came into existence about 12 million years ago and were the Proailurus.","length":91}';
+// let jsonRes = 
+// '{"fact":"The first true cats came into existence about 12 million years ago and were the Proailurus.","length":91}';
 
-let validRes = JSON.parse(jsonRes);
-console.log("->",validRes.fact);
+// let validRes = JSON.parse(jsonRes);
+// console.log("->",validRes.fact);
 
-let heading = document.createElement("p");
-heading.style.fontFamily = 'cursive';
-heading.innerText = "-> "+validRes.fact;
-document.body.appendChild(heading);
+// let heading = document.createElement("p");
+// heading.style.fontFamily = 'cursive';
+// heading.innerText = "-> "+validRes.fact;
+// document.body.appendChild(heading);
 
 
 
-// Js object -> JSON
+// // Js object -> JSON
 
-let student = {
-    name: "Rahul",
-    marks: 100,
-    city: "Haryana"
-};
+// let student = {
+//     name: "Rahul",
+//     marks: 100,
+//     city: "Haryana"
+// };
 
-let Tojson = JSON.stringify(student);
-console.log(Tojson);
+// let Tojson = JSON.stringify(student);
+// console.log(Tojson);
+
+
+
+// //Ajax , http verbs(Hoppscothch.io)
+// //first request 
+
+// let url = "https://catfact.ninja/fact";
+
+// fetch(url)
+// .then((res) =>{
+//     // console.log(res);
+//     // res.json().then((data) => console.log(data.fact));
+//     return res.json();
+// })
+// .then((data)=>{
+//     console.log("Data 1: ",data.fact);
+//     return fetch(url);
+// })
+// .then((res) =>{
+//     return res.json();
+// })
+// .then((data2) =>{
+//     console.log("Data 2: ",data2.fact);
+// })
+// .catch((error) => {
+//     console.log(error);
+// })
+
+
+//async and await function with fetch (commenting all the upper code)
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts(){
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+    }
+    catch(err){
+        console.log(err);
+    }
+      
+}
+
+getFacts();
